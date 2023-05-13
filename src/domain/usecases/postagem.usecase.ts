87c8c3451f56b6f96b769b2a1f8702ccb30c4c9f
Kevin-Sarges <kevinsarges08@gmail.com>
2023-05-13 19:19:51 -0300

    feat: criando casos de usuo

---
+++ src/domain/usecases/postagem.usecase.ts
@@ -0,0 +1,35 @@
+import { PostagemEntity } from "../entities/postagem.entity";
+import { PostagemRepository } from "../repositories/postagem.repository";
+
+export class PostagemUseCase {
+  private postagemRepository: PostagemRepository;
+
+  constructor(postagemRepository: PostagemRepository) {
+    this.postagemRepository = postagemRepository;
+  }
+
+  async criarPostagem(postagem: PostagemEntity): Promise<PostagemEntity> {
+    return this.postagemRepository.criandoPostagem(postagem);
+  }
+
+  async buscarPostagemPoId(id: string): Promise<PostagemEntity | null> {
+    return this.postagemRepository.buscarPostagemPorId(id);
+  }
+
+  async buscarPostagensPorUsuario(
+    usuarioId: string
+  ): Promise<PostagemEntity[]> {
+    return this.postagemRepository.buscandoPostagemPorUsuario(usuarioId);
+  }
+
+  async atualizarPostagem(
+    id: string,
+    postagem: PostagemEntity
+  ): Promise<PostagemEntity | null> {
+    return this.postagemRepository.atualizarPostagem(id, postagem);
+  }
+
+  async deletarPostagem(id: string): Promise<boolean> {
+    return this.postagemRepository.deletarPostagem(id);
+  }
+}
