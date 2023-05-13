8427dc189b8fa1a84bd091093a1635eab3ce2024
Kevin-Sarges <kevinsarges08@gmail.com>
2023-05-13 20:43:36 -0300

    feat: criando rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,3 @@
 export { default as CidadeRoutes } from "./cidade.routes";
 export { default as UsuarioRoutes } from "./usuarios.routes";
+export { default as PostagemRoutes } from "./postagem.routes";
---
+++ src/routes/postagem.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { PostagemRepository } from "../domain/repositories/postagem.repository";
+import { PostagemUseCase } from "../domain/usecases/postagem.usecase";
+import { PostagemController } from "../controllers/postagem.controller";
+
+const postagemRoutes = Router();
+
+const postagemRepository = new PostagemRepository();
+const postagemUsecase = new PostagemUseCase(postagemRepository);
+const postagemController = new PostagemController(postagemUsecase);
+
+postagemRoutes.post(
+  "/usuarios/usuario/:id_usuario/posatgem/criando_postagem",
+  postagemController.criandoPostagem.bind(postagemController)
+);
+postagemRoutes.get(
+  "/postagens",
+  postagemController.buscarPostagens.bind(postagemController)
+);
+postagemRoutes.get(
+  "/postagens/postagem/:id",
+  postagemController.buscarPostagemPorId.bind(postagemController)
+);
+postagemRoutes.get(
+  "/usuarios/usuario/:id_usuario/postagens",
+  postagemController.buscarPostagemPorUsuario.bind(postagemController)
+);
+postagemRoutes.put(
+  "/usuarios/usuario/:id_usuario/posatgens/postagem/:id",
+  postagemController.atualizandoPostagem.bind(postagemController)
+);
+postagemRoutes.delete(
+  "/postagens/postagem/delete/:id",
+  postagemController.deletarPostagem.bind(postagemController)
+);
+
+export default postagemRoutes;
